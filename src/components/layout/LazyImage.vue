cd3e9457b9b2d8dc073b50fe1f8c8bb382109e1e
devvstmagosso <fmagossovst@gmail.com>
2025-10-06 10:07:36 -0400

    ajustando a pagina inicial [FIX]

---
+++ src/components/layout/LazyImage.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="d-flex align-center justify-center fill-height">
+    <v-progress-circular
+        color="grey-lighten-4"
+        indeterminate
+    ></v-progress-circular>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -60,7 +60,7 @@
     </div>
 
     <!-- DIREITA -->
-    <div class="background-blue-opacity rounded-xl pa-4">
+    <div class="background-secondary rounded-xl pa-4">
       <h3 class="text-xl font-bold text-gray-900">
         Transparência e confiança
       </h3>
@@ -72,54 +72,27 @@
       <div class="mt-6 flex justify-center">
         <v-img
             :src="require('@/assets/img/quem-somos.png')"
+            :lazy-src="require('@/assets/img/quem-somos.png')"
             alt="Sobre nós"
             class="rounded-lg"
             max-width="400"
             cover
-        />
+        >
+          <template v-slot:placeholder><lazy-image /></template>
+        </v-img>
       </div>
     </div>
-  </section>
-
-  <!-- SESSÃO PARCEIROS -->
-  <section
-      data-aos="fade-right" data-aos-duration="1200"
-      class="w-100 background-secondary mt-10 flex items-center flex-col justify-center pa-5 overflow-hidden"
-  >
-    <div class="flex items-center justify-center gap-5 w-100 text-center">
-      <div class="w-[15%] h-[2px] background-blue-dark rounded-full opacity-30" />
-      <p class="text-lg opacity-90">Utilizada por empresas de todos os segmentos e tamanhos</p>
-      <div class="w-[15%] h-[2px] background-blue-dark rounded-full opacity-30" />
-    </div>
-
-    <div class="relative overflow-hidden mt-10">
-      <!-- faixa rolando -->
-      <div
-          class="flex animate-scroll gap-16 items-center"
-      >
-        <!-- Repete os logos -->
-        <img v-for="(logo, i) in logos" :key="i"
-             :src="logo"
-             class="h-12 object-contain grayscale hover:grayscale-0 transition"
-             alt="Parceiro"
-        />
-        <!-- duplicar para loop infinito -->
-        <img v-for="(logo, i) in logos" :key="'dup-' + i"
-             :src="logo"
-             class="h-9 object-contain grayscale hover:grayscale-0 transition"
-             alt="Parceiro"
-        />
-      </div>
-    </div>
-  </section>
-
-  <section class="mt-10">
-    <v-sheet data-aos="fade-right" data-aos-duration="1200" class="w-[100%] pt-9 background-blue-opacity rounded-lg">
-      <v-img class="md:w-[80%] w-[90%] rounded-sm mx-auto" :src="require('@/assets/img/foto-6.png')" />
-    </v-sheet>
   </section>
 
   <!-- SESSÃO MODULOS -->
+  <section class="mt-10">
+    <v-sheet data-aos="fade-right" data-aos-duration="1200" class="w-[100%] pt-9 background-blue-opacity rounded-lg">
+      <v-img class="md:w-[80%] w-[90%] rounded-sm mx-auto" :src="require('@/assets/img/foto-6.png')" :lazy-src="require('@/assets/img/foto-6.png')">
+        <template v-slot:placeholder><lazy-image /></template>
+      </v-img>
+    </v-sheet>
+  </section>
+
   <section class="mt-10 w-[90%] md:w-[80%] mx-auto flex flex-col items-center justify-center">
     <h2 data-aos="fade-right" data-aos-duration="1500" class="text-4xl mt-10 font-extrabold text-capitalize text-gray-900 letras-espaco text-center">
       Conheça nossos <span class="text-blue-light">Módulos</span>.
@@ -169,17 +142,21 @@
     </div>
   </section>
 
-  <section class="w-[90%] md:w-[80%] mx-auto background-blue-opacity flex flex-col lg:flex-row items-stretch px-6 py-12 rounded-xl gap-8">
+  <!-- SESSÃO APP MOBILE -->
+  <section class="px-16 background-blue-opacity flex flex-col md:flex-row items-start px-6 pt-10 rounded-xl gap-8">
     <!-- Mockup do App -->
     <v-img
         data-aos="fade-right" data-aos-duration="1500"
         :src="require('@/assets/img/celular.png')"
+        :lazy-src="require('@/assets/img/celular.png')"
         alt="App Mobile ERP"
-        class="rounded-lg mx-auto w-[70%] lg:w-[35%]"
-    />
+        class="rounded-lg mx-auto w-[70%] md:w-[40%] lg:w-[35%]"
+    >
+      <template v-slot:placeholder><lazy-image /></template>
+    </v-img>
 
     <!-- Conteúdo -->
-    <div data-aos="fade-right" data-aos-duration="1500" class="lg:w-[65%] flex flex-col justify-center">
+    <div data-aos="fade-right" data-aos-duration="1500" class="lg:w-[65%] flex flex-col justify-center mb-4">
       <p class="text-blue-light uppercase text-lg font-semibold">
         Controle na palma da sua mão
       </p>
@@ -188,54 +165,123 @@
       </h2>
 
       <!-- Cards de Benefícios -->
-      <div class="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-8">
+      <div class="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-8">
         <!-- Card -->
         <v-card
             v-for="(beneficio, i) in beneficios" :key="i"
             class="rounded-2xl shadow-md bg-white card-custom"
             elevation="0"
         >
-          <div class="flex items-center gap-5">
-            <v-btn variant="tonal" :icon="beneficio.icon" color="var(--color-blue-light)" />
-            <h3 class="font-semibold text-lg text-gray-900">{{ beneficio.titulo }}</h3>
-          </div>
-
+          <v-btn variant="tonal" :icon="beneficio.icon" color="var(--color-blue-light)" />
+          <h3 class="font-semibold text-lg text-gray-900 mt-2">{{ beneficio.titulo }}</h3>
           <p class="mt-2 text-gray-600 text-sm leading-relaxed">{{ beneficio.desc }}</p>
         </v-card>
       </div>
 
       <!-- Botões -->
-      <div class="flex items-center gap-4 mt-7 flex-col md:flex-row">
+      <div class="flex items-center gap-4 mt-7 flex-wrap">
         <v-btn
-            color="var(--color-blue-medium)" variant="flat" size="x-large"
-            class="px-6 text-white text-lg w-[100%] md:w-[50%] text-none flex items-center justify-center"
-            href="#contato"
+            color="var(--color-blue-medium)" variant="flat" size="large"
+            class="px-6 text-white text-lg text-none flex items-center justify-center"
+            href="#contato" rounded
         >
           <v-icon icon="mdi-google-play" size="30" />
           <div class="ml-3">
-            <span class="text-sm font-light">Disponível no</span>
+            <small class="text-sm font-light">Disponível no</small>
             <p>Google Play</p>
           </div>
         </v-btn>
 
         <v-btn
-            color="var(--color-blue-medium)" variant="flat" size="x-large"
-            class="px-6 text-white text-lg w-[100%] md:w-[50%] text-none flex items-center justify-center"
-            href="#contato"
+            color="var(--color-blue-medium)" variant="flat" size="large"
+            class="px-6 text-white text-lg text-none flex items-center justify-center"
+            href="#contato" rounded
         >
           <v-icon icon="mdi-apple" size="30" />
           <div class="ml-3">
-            <span class="text-sm font-light">Disponível no</span>
+            <small class="text-sm font-light">Disponível no</small>
             <p>App Store</p>
           </div>
         </v-btn>
       </div>
     </div>
   </section>
+
+  <!-- SESSÃO SERVIÇOS -->
+  <section class="mt-10 w-[90%] md:w-[80%] mx-auto flex flex-col items-center justify-center">
+    <h2 data-aos="fade-right" data-aos-duration="1500" class="text-4xl mt-10 font-extrabold text-capitalize text-gray-900 letras-espaco text-center">
+      Conheça nossos <span class="text-blue-light">Serviços</span>.
+    </h2>
+    <p data-aos="fade-right" data-aos-duration="1500" class="mt-4 text-gray-600 text-center max-w-3xl">
+      Oferecemos uma gama completa de serviços para garantir que sua
+      empresa aproveite ao máximo nossas soluções de ERP.
+    </p>
+
+    <div data-aos="fade-right" data-aos-duration="1500" class="mt-10 grid grid-cols-1 md:grid-cols-2 gap-6 items-stretch">
+      <v-card
+          v-for="(card, i) in cards"
+          :key="i"
+          class="relative overflow-hidden bg-transparent card-custom-2"
+          elevation="0"
+      >
+        <!-- Imagem -->
+        <img
+            :src="card.img"
+            :alt="card.titulo"
+            class="h-56 w-full object-cover"
+        />
+
+        <!-- Conteúdo sobreposto -->
+        <div
+            class="bg-white mx-4 relative z-10 bottom-10 shadow-md pa-4 flex flex-col flex-grow items-stretch"
+        >
+          <h3 class="font-bold text-lg text-gray-900">{{ card.titulo }}</h3>
+          <p class="text-gray-600 mt-2 flex-grow text-justify">
+            {{ card.desc }}
+          </p>
+        </div>
+      </v-card>
+    </div>
+  </section>
+
+  <!-- SESSÃO PARCEIROS -->
+  <section
+      data-aos="fade-right" data-aos-duration="1200"
+      class="w-100 background-secondary mt-10 flex items-center flex-col justify-center pa-5 overflow-hidden"
+  >
+    <div class="flex items-center justify-center gap-5 w-100 text-center">
+      <div class="w-[15%] h-[2px] background-blue-dark rounded-full opacity-30" />
+      <p class="text-lg opacity-90">Utilizada por empresas de todos os segmentos e tamanhos</p>
+      <div class="w-[15%] h-[2px] background-blue-dark rounded-full opacity-30" />
+    </div>
+
+    <div class="relative overflow-hidden mt-10">
+      <!-- faixa rolando -->
+      <div
+          class="flex animate-scroll gap-16 items-center"
+      >
+        <!-- Repete os logos -->
+        <img v-for="(logo, i) in logos" :key="i"
+             :src="logo"
+             class="h-12 object-contain grayscale hover:grayscale-0 transition"
+             alt="Parceiro"
+        />
+        <!-- duplicar para loop infinito -->
+        <img v-for="(logo, i) in logos" :key="'dup-' + i"
+             :src="logo"
+             class="h-9 object-contain grayscale hover:grayscale-0 transition"
+             alt="Parceiro"
+        />
+      </div>
+    </div>
+  </section>
+
+  <!-- SESSÃO CONTATO -->
 </template>
 
 <script setup>
 import {ref} from "vue";
+import LazyImage from "@/components/layout/LazyImage.vue";
 
 const logos = ref([
   require('@/assets/img/parceiros/fermat.png'),
@@ -292,7 +338,7 @@ const beneficios = ref([
     titulo: "Integração total",
     desc: "Conectado aos módulos do seu ERP sem complicações."
   }
-])
+]);
 
 const modulos = ref([
   { titulo: 'Estoque', descricao: 'Monitore e controle o estoque em tempo real, evitando faltas ou excessos.', icon: 'mdi-warehouse' },
@@ -301,7 +347,40 @@ const modulos = ref([
   { titulo: 'Ordem de Produção', descricao: 'Controle e organize todo o processo produtivo da sua empresa.', icon: 'mdi-factory' },
   { titulo: 'Ordem de Serviço', descricao: 'Gerencie solicitações, execuções e entregas de serviços com eficiência.', icon: 'mdi-clipboard-text-outline' },
   { titulo: 'Vendas', descricao: 'Gerencie todo o processo de vendas, desde o pedido até a entrega ao cliente.', icon: 'mdi-chart-line' },
-])
+]);
+
+const cards = ref([
+  {
+    img: require('@/assets/img/servicos/Colegas de trabalho.avif'),
+    titulo: "Consultoria de Gestão",
+    desc: "Orientamos sua empresa na tomada de decisões estratégicas, identificando oportunidades de crescimento e melhorias nos processos. Alavanque seu negócio com nossa expertise."
+  },
+  {
+    img: require('@/assets/img/servicos/reunião.avif'),
+    titulo: "Suporte e Manutenção",
+    desc: "Oferecemos suporte técnico especializado e serviços de manutenção para garantir o bom funcionamento dos seus sistemas e equipamentos. Conte conosco para resolver problemas de forma ágil e eficiente."
+  },
+  {
+    img: require('@/assets/img/servicos/chuva de ideias.avif'),
+    titulo: "Treinamentos",
+    desc: "Capacite sua equipe com nossos treinamentos especializados, desenvolvidos para aprimorar habilidades e conhecimentos específicos. Invista no desenvolvimento profissional e potencialize os resultados da sua empresa."
+  },
+  {
+    img: require('@/assets/img/servicos/Dando uma apresentação.avif'),
+    titulo: "Customizações",
+    desc: "Adaptamos soluções de software às necessidades exclusivas do seu negócio, personalizando funcionalidades e interfaces. Tenha um sistema sob medida para impulsionar sua produtividade e eficiência."
+  },
+  {
+    img: require('@/assets/img/servicos/codificação.avif'),
+    titulo: "Business Intelligence",
+    desc: "Transforme dados em insights valiosos com nossa solução de Business Intelligence. Tenha acesso a relatórios e análises detalhadas para embasar suas decisões estratégicas e obter vantagem competitiva."
+  },
+  {
+    img: require('@/assets/img/servicos/imagem_2023-10-09_120824967.avif'),
+    titulo: "Migração de dados entre sistemas",
+    desc: "Realizamos a migração segura e eficiente dos seus dados de um sistema para outro, garantindo a integridade e a consistência das informações. Simplifique o processo e evite perdas de dados."
+  }
+]);
 </script>
 
 <style scoped>
@@ -327,6 +406,20 @@ const modulos = ref([
   transition: transform 0.7s ease, box-shadow 0.7s ease;
 }
 .card-custom:hover {
+  transform: scale(1.05) translateY(-4px);
+  box-shadow: 0 12px 24px rgba(0,0,0,0.15);
+  transition: transform 0.7s ease, box-shadow 0.7s ease;
+}
+
+.card-custom-2 {
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  background: #fff;
+  box-shadow: 0 2px 8px rgba(0,0,0,0.08);
+  transition: transform 0.7s ease, box-shadow 0.7s ease;
+}
+.card-custom-2:hover {
   transform: scale(1.05) translateY(-4px);
   box-shadow: 0 12px 24px rgba(0,0,0,0.15);
   transition: transform 0.7s ease, box-shadow 0.7s ease;
